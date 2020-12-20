553a99641eac5175bcc79865a73533aee69d6c83
Diego Plancarte <dplancarte7@gmail.com>
2020-12-19 17:35:21 -0800

    Add sign up component to sign up page

--- src/pages/sign-in-out/SignInOut.component.jsx
+++ src/pages/sign-in-out/SignInOut.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/SignIn.component';
+import SignUp from '../../components/sign-up/SignUp.component'
 import './signinout.styles.scss'
 
 const SignInOut = () => {
   return (
-    <div>
+    <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
