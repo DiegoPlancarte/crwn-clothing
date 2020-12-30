44a9c54b417a694e5a2bd7d8eb951fafd0aa465a
Diego Plancarte <dplancarte7@gmail.com>
2020-12-30 15:59:50 -0800

    IAdded Add to Cart button on items.

--- src/components/collection-item/CollectionItem.component.jsx
+++ src/components/collection-item/CollectionItem.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './collectionitem.styles.scss'
 
+import CustomButton from '../custom-button/CustomButton.component'
+
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
     <div className='collection-item'>
       <div 
-        style={{ backgroundImage: `url(${imageUrl})`}}
         className='image'
+        style={{ backgroundImage: `url(${imageUrl})`}}
       />
       <div className='collection-footer'>
         <span className='name' >{ name }</span>
         <span className='price' >{ price }</span>
       </div>
+      <CustomButton inverted>ADD TO CART</CustomButton>
     </div>
   );
 };
