1838c361531f6def509f1afb545b17bbd6ef6ca1
Diego Plancarte <dplancarte7@gmail.com>
2020-12-30 15:10:11 -0800

    Create cart actions, reducer, and types. Implemented redux for cart to show and hide

--- src/components/cart-icon/CartIcon.component.jsx
+++ src/components/cart-icon/CartIcon.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import './carticon.styles.scss'
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
+import { connect } from 'react-redux';
 
-const CartIcon = () => {
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
+import './carticon.styles.scss';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
+const CartIcon = ({ toggleCartHidden}) => {
   return (
-    <div className='cart-icon'>
+    <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon' />
       <span className='item-count'>0</span>
     </div>
   );
 };
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/CartIcon.component';
 import CartDropdown from '../cart-dropdown/CartDropdown.component'
 
-const Header = ({currentUser}) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -26,13 +26,14 @@ const Header = ({currentUser}) => {
         }
         <CartIcon />
       </div>
-      <CartDropdown />
+      { hidden ? null : <CartDropdown /> }
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
