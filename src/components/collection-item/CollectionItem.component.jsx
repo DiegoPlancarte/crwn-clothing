d726f921a4b9b739753b9233736450906d574db7
Diego Plancarte <dplancarte7@gmail.com>
2020-12-31 07:17:26 -0800

    Created action to add item to cart. Reconfigured CollectionPreview component to send entire item to CollectionItem component.

--- src/components/collection-item/CollectionItem.component.jsx
+++ src/components/collection-item/CollectionItem.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import './collectionitem.styles.scss'
+import { connect } from 'react-redux';
+import './collectionitem.styles.scss';
 
-import CustomButton from '../custom-button/CustomButton.component'
+import CustomButton from '../custom-button/CustomButton.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className='collection-item'>
       <div 
@@ -14,9 +17,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name' >{ name }</span>
         <span className='price' >{ price }</span>
       </div>
-      <CustomButton inverted>ADD TO CART</CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
